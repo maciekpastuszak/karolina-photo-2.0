e64bc8da8240b415864343e390be6e40e32bc285
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-02 11:01:25 +0100

    add testing hook to pass data from back to front

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      } 
+    )
+  },[])
+  
   return (
     <div>App</div>
   )
